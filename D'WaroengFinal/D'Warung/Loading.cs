d1ef44edb9ab2ecf39463911669cf6b10fc5250f
JunianN <rizky.j.n@mail.ugm.ac.id>
2021-06-14 13:57:22 +0700

    Upload final version

---
+++ D'WaroengFinal/D'Warung/Loading.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_Warung
+{
+    public partial class Loading : Form
+    {
+        public Loading()
+        {
+            InitializeComponent();
+        }
+
+        int startpoint = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            startpoint += 1;
+            Myprogress.Value = startpoint;
+            if (Myprogress.Value == 100)
+            {
+                Myprogress.Value = 0;
+                timer1.Stop();
+                Form1 log = new Form1();
+                this.Hide();
+                log.Show();
+            }
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
